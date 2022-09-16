4e03471c2a91e106dbea2a08fd39719c85e4b53a
Dearian27 <105427745+Dearian27@users.noreply.github.com>
2022-09-17 00:02:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,6 @@
 function toAcronym(inp){
   return inp.split(' ') // str to array 'Code wars' to ['Code', 'wars']
   .map(el =>el[0].toUpperCase()) /* first letter of every element ['C', 'w'] 
-  and use methor toUpperCase to make it ['C', 'W']  */
+  and use methor 'toUpperCase' to make it ['C', 'W']  */
   .join('') // concatenation els of array without any connection symbols
 }
